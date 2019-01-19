0d0de9be48a90cf1a91065dbd76a52a6e5ebc933
Ryan Apking <ryan.apking@gmail.com>
2019-01-18 16:28:02 -0800

    Moving around some back4app logic.

--- data/utilities/index.js
+++ data/utilities/index.js
@@ -1,9 +1,9 @@
 // logic has been moved into the back4app directory to prevent the need for duplicated code
 // exported here for convenience
 
-export { generateBoard, generatePiece, generateLocalPiece, generateGame, getRandomLetter } from '../b4a-cloud-functions/cloud/utilities/functions/generators';
-export { remoteToLocal, localToRemote, wordPathArrayToString, boardStringToArray, pieceStringToArray, wordPathStringToArray, challengeRemoteToLocal, challengeMoveToHistory } from '../b4a-cloud-functions/cloud/utilities/functions/dataConversions';
-export { calculateWordValue, calculateHighestWordValue, calculateLongestWordLength, calculateMoveRating } from '../b4a-cloud-functions/cloud/utilities/functions/calculations';
-export { checkPieceFit, scoreTabulator, getWinner } from '../b4a-cloud-functions/cloud/utilities/functions/checks';
-export { getScoreBoard, getAnimationData, getWordPath } from '../b4a-cloud-functions/cloud/utilities/functions/getters';
-export { letterValues } from '../b4a-cloud-functions/cloud/utilities/config';
+export { generateBoard, generatePiece, generateLocalPiece, generateGame, getRandomLetter } from '../back4app/cloud/utilities/functions/generators';
+export { remoteToLocal, localToRemote, wordPathArrayToString, boardStringToArray, pieceStringToArray, wordPathStringToArray, challengeRemoteToLocal, challengeMoveToHistory } from '../back4app/cloud/utilities/functions/dataConversions';
+export { calculateWordValue, calculateHighestWordValue, calculateLongestWordLength, calculateMoveRating } from '../back4app/cloud/utilities/functions/calculations';
+export { checkPieceFit, scoreTabulator, getWinner } from '../back4app/cloud/utilities/functions/checks';
+export { getScoreBoard, getAnimationData, getWordPath } from '../back4app/cloud/utilities/functions/getters';
+export { letterValues } from '../back4app/cloud/utilities/config';
