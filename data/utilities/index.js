9e9ec92c1a7a5369f57adedad01e1bffec31cf24
Ryan Apking <ryan.apking@gmail.com>
2019-01-22 21:05:56 -0800

    Add a next piece generator and utility to decode next piece into a piece of a certain size.

--- data/utilities/index.js
+++ data/utilities/index.js
@@ -2,7 +2,7 @@
 // exported here for convenience
 
 export { generateBoard, generatePiece, generateLocalPiece, generateGame, getRandomLetter } from '../back4app/cloud/utilities/functions/generators';
-export { remoteToLocal, localToRemote, wordPathArrayToString, boardStringToArray, pieceStringToArray, wordPathStringToArray, challengeRemoteToLocal, challengeMoveToHistory } from '../back4app/cloud/utilities/functions/dataConversions';
+export { remoteToLocal, localToRemote, wordPathArrayToString, boardStringToArray, pieceStringToArray, wordPathStringToArray, challengeRemoteToLocal, challengeMoveToHistory, nextPieceStringToLocalPiece, nextPieceStringToRemotePiece } from '../back4app/cloud/utilities/functions/dataConversions';
 export { calculateWordValue, calculateHighestWordValue, calculateLongestWordLength, calculateMoveRating } from '../back4app/cloud/utilities/functions/calculations';
 export { checkPieceFit, scoreTabulator, getWinner, validateMove } from '../back4app/cloud/utilities/functions/checks';
 export { getScoreBoard, getAnimationData, getWordPath } from '../back4app/cloud/utilities/functions/getters';
