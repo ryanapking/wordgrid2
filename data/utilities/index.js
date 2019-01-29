01cf958aff08beffe1c6348a476a42b3851a0a3a
Ryan Apking <ryan.apking@gmail.com>
2019-01-28 21:08:17 -0800

    Update the GameReview logic to process the history based on the starting point and the applied moves.

--- data/utilities/index.js
+++ data/utilities/index.js
@@ -2,8 +2,9 @@
 // exported here for convenience
 
 export { generateBoard, generatePiece, generateLocalPiece, generateGame, getRandomLetter } from '../back4app/cloud/utilities/functions/generators';
-export { remoteToLocal, localToRemote, wordPathArrayToString, boardStringToArray, arrayToString, pieceStringToArray, wordPathStringToArray, challengeRemoteToLocal, challengeMoveToHistory, nextPieceStringToLocalPiece, nextPieceStringToRemotePiece } from '../back4app/cloud/utilities/functions/dataConversions';
+export { remoteToStartingGameState, remoteToLocal, localToRemote, wordPathArrayToString, boardStringToArray, arrayToString, pieceStringToArray, wordPathStringToArray, challengeRemoteToLocal, challengeMoveToHistory, moveRemoteToLocal, nextPieceStringToLocalPiece, nextPieceStringToRemotePiece } from '../back4app/cloud/utilities/functions/dataConversions';
 export { calculateWordValue, calculateHighestWordValue, calculateLongestWordLength, calculateMoveRating } from '../back4app/cloud/utilities/functions/calculations';
 export { checkPieceFit, getWinner, validateMove } from '../back4app/cloud/utilities/functions/checks';
 export { getAnimationData, getWordPath } from '../back4app/cloud/utilities/functions/getters';
+export { applyMove } from '../back4app/cloud/utilities/functions/applyMoves';
 export { letterValues } from '../back4app/cloud/utilities/config';
