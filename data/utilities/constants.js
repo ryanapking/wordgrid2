f5166d1e8227a0f190ac9c79c24a0801cffc6504
Ryan Apking <ryan.apking@gmail.com>
2019-07-29 16:29:20 -0600

    Pulling changes to utilities from bit.

--- data/utilities/constants.js
+++ data/utilities/constants.js
@@ -1,5 +1,5 @@
 // used for styling tile spaces in the RN application
-export const SPACE_STATES = {
+const SPACE_STATES = {
   SPACE_EMPTY: 0,
   SPACE_FILLED: 1,
   SPACE_CONSUMED: 2,
@@ -8,7 +8,7 @@ export const SPACE_STATES = {
 };
 
 // Status strings are stored in the DB. Do not change.
-export const GAME_STATES = {
+const GAME_STATES = {
   NEW: "new", // set upon creation, will never be set to this on game save. Game has no player2.
   REQUEST_PENDING_NEW: "rp-new", // game was initialized with a specific opponent, waiting for requester to make first move
   REQUEST_PENDING: "rp", // game was initialized with a specific opponent, waiting for confirmation from opponent
@@ -19,3 +19,7 @@ export const GAME_STATES = {
   FORFEIT: "forfeit", // game was forfeit
 };
 
+module.exports = {
+  SPACE_STATES,
+  GAME_STATES,
+};
