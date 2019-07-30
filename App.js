0a982f1b3f575398d0db29a0f66bb0a4891bc35b
Ryan Apking <ryan.apking@gmail.com>
2019-07-30 13:41:25 -0600

    Move the route declaration out of App.js and into an iterable object to allow for more data to be stored with the route info.

--- App.js
+++ App.js
@@ -5,28 +5,11 @@ import { NativeRouter, Route } from'react-router-native';
 import { Overlay } from 'react-native-elements';
 
 import configureStore from './data/redux/configureStore';
-
-// routes
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import Account from "./routes/Account";
-import Friends from "./routes/Friends";
-import Friend from "./routes/Friend";
-import FriendArchive from "./routes/FriendArchive";
-import Game from "./routes/Game";
-import GameArchive from "./routes/GameArchive";
-import GameReview from './routes/GameReview';
-import Games from "./routes/Games";
-import Challenges from "./routes/Challenges";
-import Challenge from "./routes/Challenge";
-import ChallengeAttempt from "./routes/ChallengeAttempt";
-import ChallengeAttemptReview from "./routes/ChallengeAttemptReview";
-
-// redirect all non-logged in users to the login screen
-import LoginRedirect from "./components/nondisplay/LoginRedirect";
+import { routesArray, RouteComponents } from './routes';
 import Menu from './components/Menu';
 import TopBar from './components/TopBar';
 import MessageOverlay from './components/MessageOverlay';
+import LoginRedirect from "./components/nondisplay/LoginRedirect";
 
 const store = configureStore();
 
@@ -55,21 +38,14 @@ export default class App extends Component {
                 <TopBar openDrawer={() => this.setState({ menuOverlayVisible: true })} />
               </View>
               <View style={styles.mainSection}>
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/account" component={Account} />
-                <Route path="/friends" component={Friends} />
-                <Route exact path="/friend/:friendID" component={Friend} />
-                <Route path="/friend/:friendID/archive" component={FriendArchive} />
-                <Route path="/game/:gameID" component={Game} />
-                <Route path="/games/archive" component={GameArchive} />
-                <Route path="/gameReview/:gameID" component={GameReview} />
-                <Route exact path="/games" component={Games} />
-                <Route path="/challenges" component={Challenges} />
-                <Route path="/challenge/:challengeID" component={Challenge} />
-                <Route path="/challengeAttempt" component={ChallengeAttempt} />
-                <Route exact path="/challengeAttemptReview/:attemptID" component={ChallengeAttemptReview} />
-                <Route exact path="/challengeAttemptReview/:challengeID/:attemptIndex" component={ChallengeAttemptReview} />
+                { routesArray.map((route, index) =>
+                  <Route
+                    key={index}
+                    exact
+                    path={route.path}
+                    component={RouteComponents[route.componentName]}
+                  />
+                )}
               </View>
             </View>
             <LoginRedirect />
---
+++ routes/index.js
@@ -0,0 +1,112 @@
+import Home from "./Home";
+import Login from "./Login";
+import Account from "./Account";
+import Friends from "./Friends";
+import Friend from "./Friend";
+import FriendArchive from "./FriendArchive";
+import Game from "./Game";
+import GameArchive from "./GameArchive";
+import GameReview from './GameReview';
+import Games from "./Games";
+import Challenges from "./Challenges";
+import Challenge from "./Challenge";
+import ChallengeAttempt from "./ChallengeAttempt";
+import ChallengeAttemptReview from "./ChallengeAttemptReview";
+
+export const RouteComponents = {
+  Home,
+  Login,
+  Account,
+  Friends,
+  Friend,
+  FriendArchive,
+  Game,
+  GameArchive,
+  GameReview,
+  Games,
+  Challenges,
+  Challenge,
+  ChallengeAttempt,
+  ChallengeAttemptReview,
+};
+
+const routes = {
+  home: {
+    routeTitle: "Games",
+    backRoute: null,
+    path: "/",
+    componentName: "Games",
+  },
+  game: {
+    routeTitle: "vs ${opponentName}",
+    backRoute: "home",
+    path: "/game/:gameID",
+    componentName: "Game",
+  },
+  gameReview: {
+    routeTitle: "vs ${opponentName} Review",
+    backRoute: "${prevHistory}",
+    path: "/gameReview/:gameID",
+    componentName: "GameReview",
+  },
+  challenges: {
+    routeTitle: "Challenges",
+    backRoute: "home",
+    path: "/challenges",
+    componentName: "Challenges",
+  },
+  challenge: {
+    routeTitle: "${challengeDate} Challenge",
+    backRoute: "challenges",
+    path: "/challenge/:challengeID",
+    componentName: "Challenge",
+  },
+  challengeAttempt: {
+    routeTitle: "${challengeDate} Challenge Attempt",
+    backRoute: "${prevHistory}",
+    path: "/challengeAttempt",
+    componentName: "ChallengeAttempt",
+  },
+  challengeAttemptReview: {
+    routeTitle: "Complex Title",
+    backRoute: "${prevHistory}",
+    path: "/challengeAttemptReview/:attemptID",
+    componentName: "ChallengeAttemptReview",
+  },
+  login: {
+    routeTitle: "Login",
+    backRoute: null,
+    path: "/login",
+    componentName: "Login",
+  },
+  account: {
+    routeTitle: "Account",
+    backRoute: "home",
+    path: "/account",
+    componentName: "Account",
+  },
+  friends: {
+    routeTitle: "Friends",
+    backRoute: "home",
+    path: "/friends",
+    componentName: "Friends",
+  },
+  friend: {
+    routeTitle: "${friendName}",
+    backRoute: "friends",
+    path: "/friend/:friendID",
+    componentName: "Friend",
+  },
+  friendArchive: {
+    routeTitle: "${friendName} Archive",
+    backRoute: "friend",
+    path: "/friend/:friendID/archive",
+    componentName: "FriendArchive",
+  },
+};
+
+export const routesArray = Object.keys(routes).map((routeName) => {
+  return routes[routeName];
+});
+
+export default routes;
