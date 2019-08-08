2dab977946a56d2458238cd41c6f0410f05f0dcc
Ryan Apking <ryan.apking@gmail.com>
2019-08-08 14:13:13 -0600

    Converting the Game route to a functional component.

--- routes/Game.js
+++ routes/Game.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-native';
+import React, { useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
 import Boggle from '../data/boggle-solver';
-import BoardTouchView from '../components/BoardTouchView';
-import GameMoveAnimation from '../components/GameMoveAnimation';
-import PieceOverlay from '../components/PieceOverlay';
+import routes from '../routes';
 import {
   setAvailableWordsData,
   consumeSquare,
@@ -20,8 +17,13 @@ import {
   calculateHighestWordValue,
   calculateLongestWordLength,
 } from "../data/utilities/functions/calculations";
+import { saveMove } from "../data/redux/thunkedGameActions";
 import { checkPieceFit } from "../data/utilities/functions/checks";
+import { useParams, useHistory } from "../hooks/tempReactRouter";
 
+import BoardTouchView from '../components/BoardTouchView';
+import GameMoveAnimation from '../components/GameMoveAnimation';
+import PieceOverlay from '../components/PieceOverlay';
 import SpellWordSection from "../components/SpellWordSection";
 import BoardDrawLetterGrid from "../components/BoardDrawLetterGrid";
 import BoardPathCreator from "../components/BoardPathCreator";
@@ -29,9 +31,14 @@ import DrawPieceSection from "../components/DrawPieceSection";
 import GameScoreBoard from "../components/GameScoreBoard";
 import GamePhaseDisplay from "../components/GamePhaseDisplay";
 
-class Game extends Component {
+const Game = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const params = useParams();
+  const { gameID } = params;
+  const [uid, game, display] = useSelector(state => [state.user.uid, state.gameData.byID[gameID], state.gameDisplay], shallowEqual);
 
-  componentDidMount() {
+  useEffect(() => {
     // console.log('game.js component mounted');
     // console.log('game:', this.props.game);
 
@@ -54,118 +61,112 @@ class Game extends Component {
     // });
 
     // will need to be fixed later. removing history reference.
-    const currentBoardString = this.props.game.sourceData.startingBoard;
+    const currentBoardString = game.sourceData.startingBoard;
     let boggle = new Boggle(currentBoardString);
 
     boggle.solve( (words) => {
       const longest = calculateLongestWordLength(words).length;
       const mostValuable = calculateHighestWordValue(words).value;
 
-      this.props.setAvailableWordsData(this.props.gameID, longest, mostValuable, words.length);
+      dispatch(setAvailableWordsData(gameID, longest, mostValuable, words.length));
     });
+  }, []);
 
-  }
-
-  render() {
-    const { game, gameID, uid } = this.props;
-
-    // zindex doesn't work on android, so we need to move the overlay before or after the other visual elements
-    const overlayActive = (game.word && !game.piecePlaced);
-
-    const pieceOverlay = (
-      <PieceOverlay
-        pointerEvents={'none'}
-        boardRows={game.rows}
-        placePiece={(pieceIndex, rowRef, columnRef) => this.props.placePiece(this.props.gameID, pieceIndex, rowRef, columnRef)}
-      />
-    );
-
-    if (!this.props.game.animationOver) {
-      return (
-        <View style={styles.container}>
-          <GameMoveAnimation game={game} gameID={gameID} />
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          { overlayActive ? null : pieceOverlay }
-          <View style={styles.underlay}>
-            <View style={styles.info}>
-              <View style={[styles.leftSide, {padding: 5}]}>
-                <Text>Opponent Pieces:</Text>
-                <DrawPieceSection pieces={game.opponent.currentPieces} allowDrag={false} />
-              </View>
-              <View style={[styles.rightSide, {padding: 5}]}>
-                <GameScoreBoard
-                  style={styles.equal}
-                  uid={uid}
-                  p1={game.p1}
-                  p2={game.p2}
-                  currentPlayerScoreBoard={game.currentPlayer.scoreBoard}
-                  opponentScoreBoard={game.opponent.scoreBoard}
-                />
-                <GamePhaseDisplay style={styles.equal} movePhase={game.movePhase} />
-              </View>
-            </View>
-            <BoardTouchView
-              style={styles.board}
-              rows={game.rows}
-              pointerEventsDisabled={game.word.length > 0}
-              consumedSquares={game.consumedSquares}
-              consumeSquare={(square) => this.props.consumeSquare(square, gameID)}
-              removeSquare={() => this.props.removeSquare(gameID)}
-              clearConsumedSquares={() => this.props.clearConsumedSquares(gameID)}
-            >
-              <BoardDrawLetterGrid
-                boardState={game.rows}
-                boardSize={this.props.display.boardLocation.width}
-                consumedSquares={game.consumedSquares}
-                hoveredSquares={this.props.display.hoveredSpaces}
-              />
-              <BoardPathCreator
-                squares={game.consumedSquares}
-                boardLocation={this.props.display.boardLocation}
-              />
-            </BoardTouchView>
-            <View style={styles.interaction}>
-              { game.movePhase === "place" ?
-                <DrawPieceSection pieces={game.currentPlayer.currentPieces} allowDrag />
-                : null
-              }
-              { game.movePhase === "confirm" ?
-                <View style={styles.confirmMoveSection}>
-                  <Button title="Submit Move" onPress={ () => this.props.saveMove(gameID, uid) } />
-                  <Button title="Reset Move" onPress={ () => this.props.resetLocalGameDataByID(gameID, uid) } />
-                </View>
-                : null
-              }
-              { game.movePhase === "spell" ?
-                <SpellWordSection
-                  playWord={ () => this.props.playWord(gameID) }
-                  clearConsumedSquares={ () => this.props.clearConsumedSquares(gameID) }
-                  displayPieces={game.currentPlayer.currentPieces}
-                  consumedSquares={game.consumedSquares}
-                />
-                : null
-              }
-            </View>
-          </View>
-          { overlayActive ? pieceOverlay : null }
-        </View>
-      );
-    }
-
-  }
-
-  componentDidUpdate() {
-    if ( this.props.uid !== this.props.game.turn ) {
-      this.props.history.push(`/games`);
+  // redirect if needed
+  useEffect(() => {
+    if ( uid !== game.turn ) {
+      history.push(routes.home.path);
       console.log("opponent's move. redirecting...");
     }
-  }
+  }, [uid, game, history]);
 
-}
+  // zindex doesn't work on android, so we need to move the overlay before or after the other visual elements
+  const overlayActive = (game.word && !game.piecePlaced);
+
+  const pieceOverlay = (
+    <PieceOverlay
+      pointerEvents={'none'}
+      boardRows={game.rows}
+      placePiece={(pieceIndex, rowRef, columnRef) => dispatch(placePiece(gameID, pieceIndex, rowRef, columnRef)) }
+    />
+  );
+
+  if (!game.animationOver) {
+    return (
+      <View style={styles.container}>
+        <GameMoveAnimation game={game} gameID={gameID} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        { overlayActive ? null : pieceOverlay }
+        <View style={styles.underlay}>
+          <View style={styles.info}>
+            <View style={[styles.leftSide, {padding: 5}]}>
+              <Text>Opponent Pieces:</Text>
+              <DrawPieceSection pieces={game.opponent.currentPieces} allowDrag={false} />
+            </View>
+            <View style={[styles.rightSide, {padding: 5}]}>
+              <GameScoreBoard
+                style={styles.equal}
+                uid={uid}
+                p1={game.p1}
+                p2={game.p2}
+                currentPlayerScoreBoard={game.currentPlayer.scoreBoard}
+                opponentScoreBoard={game.opponent.scoreBoard}
+              />
+              <GamePhaseDisplay style={styles.equal} movePhase={game.movePhase} />
+            </View>
+          </View>
+          <BoardTouchView
+            style={styles.board}
+            rows={game.rows}
+            pointerEventsDisabled={game.word.length > 0}
+            consumedSquares={game.consumedSquares}
+            consumeSquare={(square) => dispatch(consumeSquare(square, gameID)) }
+            removeSquare={() => dispatch(removeSquare(gameID)) }
+            clearConsumedSquares={() => dispatch(clearConsumedSquares(gameID)) }
+          >
+            <BoardDrawLetterGrid
+              boardState={game.rows}
+              boardSize={display.boardLocation.width}
+              consumedSquares={game.consumedSquares}
+              hoveredSquares={display.hoveredSpaces}
+            />
+            <BoardPathCreator
+              squares={game.consumedSquares}
+              boardLocation={display.boardLocation}
+            />
+          </BoardTouchView>
+          <View style={styles.interaction}>
+            { game.movePhase === "place" ?
+              <DrawPieceSection pieces={game.currentPlayer.currentPieces} allowDrag />
+              : null
+            }
+            { game.movePhase === "confirm" ?
+              <View style={styles.confirmMoveSection}>
+                <Button title="Submit Move" onPress={ () => dispatch(saveMove(gameID, uid)) } />
+                <Button title="Reset Move" onPress={ () => dispatch(resetLocalGameDataByID(gameID, uid)) } />
+              </View>
+              : null
+            }
+            { game.movePhase === "spell" ?
+              <SpellWordSection
+                playWord={ () => dispatch(playWord(gameID)) }
+                clearConsumedSquares={ () => dispatch(clearConsumedSquares(gameID)) }
+                displayPieces={game.currentPlayer.currentPieces}
+                consumedSquares={game.consumedSquares}
+              />
+              : null
+            }
+          </View>
+        </View>
+        { overlayActive ? pieceOverlay : null }
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -217,24 +218,4 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapStateToProps = (state, ownProps) => {
-  const gameID = ownProps.match.params.gameID;
-  return {
-    gameID: gameID,
-    game: state.gameData.byID[gameID],
-    uid: state.user.uid,
-    display: state.gameDisplay,
-  };
-};
-
-const mapDispatchToProps = {
-  setAvailableWordsData,
-  consumeSquare,
-  removeSquare,
-  clearConsumedSquares,
-  placePiece,
-  resetLocalGameDataByID,
-  playWord,
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Game));
+export default Game;
