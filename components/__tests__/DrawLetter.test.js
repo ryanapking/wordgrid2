90b48f5c2fd55c673a62fe03ff8f421dfb61158b
Ryan Apking <ryan.apking@gmail.com>
2019-08-12 11:03:08 -0600

    Adding some testing libraries and a basic render test for DrawLetter component to serve as an example.

---
+++ components/__tests__/DrawLetter.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DrawLetter from "../DrawLetter";
+
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const drawLetterWithSize = renderer.create(<DrawLetter letter="a" letterSize={25} />).toJSON();
+  expect(drawLetterWithSize).toMatchSnapshot();
+
+  const drawLetterWithoutSize = renderer.create(<DrawLetter letter="q" />).toJSON();
+  expect (drawLetterWithoutSize).toMatchSnapshot();
+
+  const drawLetterWithoutLetter = renderer.create(<DrawLetter />).toJSON();
+  expect (drawLetterWithoutLetter).toMatchSnapshot();
+});
